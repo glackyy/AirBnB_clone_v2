a3d79c1b35bf153823c08b5d8886462316cb0cf0
ChahirSaid <chahirsaid18@gmail.com>
2023-09-20 17:38:36 +0100

    amenity improvement

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey, Table
+from sqlalchemy.orm import relationship
+from models.place import place_amenity
+import models
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """class Amenity"""
+
+    __tablename__ = "amenities"
+
+    if models.storage_t == "db":
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary=place_amenity)
+    else:
+        name = ""
