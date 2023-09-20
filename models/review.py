6292f0d5125b671ab92f401323bc7586bdcef171
ChahirSaid <80861946+ChahirSaid@users.noreply.github.com>
2023-09-20 18:04:01 +0100

    review improvement

--- models/review.py
+++ models/review.py
@@ -8,7 +8,7 @@ import models
 class Review(BaseModel, Base):
     """Review classto store review information"""
 
-    __tablename__ = "reviews"):
+    __tablename__ = "reviews"
     if models.storage_t == "db":
         place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
