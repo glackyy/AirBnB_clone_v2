c704f5c074b95efb26489b483811dd7ad0e946ec
ChahirSaid <chahirsaid18@gmail.com>
2023-09-20 17:51:19 +0100

    user improvement

--- models/user.py
+++ models/user.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+import models
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+
+    __tablename__ = "users"
+    if models.storage_t == "db":
+        email = Column("email", String(128), nullable=False)
+        password = Column("password", String(128), nullable=False)
+        first_name = Column("first_name", String(128), nullable=True)
+        last_name = Column("last_name", String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
