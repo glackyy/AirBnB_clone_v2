89a7c720f024efa2343c6a3449b57e34e69c48bf
ChahirSaid <80861946+ChahirSaid@users.noreply.github.com>
2023-09-20 18:07:20 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -65,8 +65,8 @@ class BaseModel:
         if dictionary.get("_sa_instance_state"):
             dictionary.pop("_sa_instance_state")
 
-        dictionary.update({'__class__':
-                          (str(type(self)).split(".")[-1]).split("'")[0]})
+        dictionary.update({"__class__":
+                           (str(type(self)).split(".")[-1]).split("'")[0]})
         dictionary["created_at"] = self.created_at.isoformat()
         dictionary["updated_at"] = self.updated_at.isoformat()
         return dictionary
