abc7e85535de1bbc4e26089770ffe57a98917aa4
ChahirSaid <chahirsaid18@gmail.com>
2023-09-20 17:49:23 +0100

    state improvement

--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column
+from sqlalchemy.orm import relationship
+from models.city import City
+import models
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """State class"""
+
+    __tablename__ = "states"
+
+    if models.storage_t == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="all, delete")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """Method that gets cities"""
+            cityList = []
+            for cityV in models.storage.all(City).values():
+                if getattr(cityV, "state_id") == self.id:
+                    cityList.append(cityV)
+            return cityList
