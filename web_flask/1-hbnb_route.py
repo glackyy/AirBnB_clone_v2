f3ac2836c16ff0c507c1db4c2739480f3b81bfcb
glackyy <othmanesticky@gmail.com>
2023-10-20 19:56:08 +0100

    hbnb routee

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,1 +1,14 @@
-#\!/usr/bin/python3
+#!/usr/bin/python3
+"""Starting flask web app"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displaying 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
