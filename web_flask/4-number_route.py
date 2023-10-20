6bec871adb5108adcb0e63b0191559a3683b310d
glackyy <othmanesticky@gmail.com>
2023-10-20 20:13:48 +0100

    number route

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,1 +1,42 @@
-#\!/usr/bin/python3
+#!/usr/bin/python3
+"""Starting Flask web app"""
+from flask import Flask
+from flask import abort
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displaying 'Hello HBNB!'."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displaying 'HBNB'."""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displaying 'C' followed by the value of <text>"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Displaying 'Python' followed by the value of <text>"""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """Displaying 'n is a number' if n is an int"""
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
