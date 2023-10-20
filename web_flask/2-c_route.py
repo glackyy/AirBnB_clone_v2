a00cd01e53e474fc12a74ce34dd9adc0aaa0ae11
glackyy <othmanesticky@gmail.com>
2023-10-20 20:04:15 +0100

    C route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,1 +1,27 @@
-#\!/usr/bin/python3
+#!/usr/bin/python3
+"""Starting flask web app"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displaying 'Hello HBNB!'."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displaying 'HBNB'."""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displaying 'C' followed by the value of <text>"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
