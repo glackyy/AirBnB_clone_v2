c7e6b59576af760e7509655290cca04f237d854a
glackyy <othmanesticky@gmail.com>
2023-10-20 20:00:09 +0100

    hbnb routee

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,7 +10,7 @@ def hello_hbnb():
     return "Hello HBNB!"
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Displaying 'HBNB'."""
     return "HBNB"
