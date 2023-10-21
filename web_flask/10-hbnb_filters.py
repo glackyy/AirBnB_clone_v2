df523b6c657344d04d40eb11ac6a5026422b0c32
glackyy <othmanesticky@gmail.com>
2023-10-21 11:06:01 +0100

    hbnb filters

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,1 +1,25 @@
-#\!/usr/bin/python3
+#!/usr/bin/python3
+"""Starting Flask web app"""
+from flask import Flask
+from flask import render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    """Displaying the Main Hbnb filters Html page"""
+    states = storage.all("State")
+    amenities = storage.all("Amenity")
+    return render_template("10-hbnb_filters.html",
+                           states=states, amenities=amenities)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Removing the current SqlAlchemy session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
