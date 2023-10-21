dfc7b7b21ee6c1cbcbc1a5205ed9e2101fc237fa
glackyy <othmanesticky@gmail.com>
2023-10-21 10:35:42 +0100

    state list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,1 +1,23 @@
-#\!/usr/bin/python3
+#!/usr/bin/python3
+"""Starting Flask web app"""
+from flask import Flask
+from flask import render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Displaying Html Page with a list of all state obj in dbstorage"""
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Removing the current SqlAlchemy session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
